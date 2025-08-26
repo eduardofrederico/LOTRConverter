858ed02aa33ff963b1d3f385c50db12cff0133f1
Eduardo Frederico <eduardo.frederico@icloud.com>
2025-08-26 00:27:19 -0300

    Ajustando o layout.

--- LOTRConverter/ContentView.swift
+++ LOTRConverter/ContentView.swift
@@ -11,11 +11,21 @@ struct ContentView: View {
     var body: some View {
         ZStack {
             // Background image
+            Image(.background)
+                .resizable()
+                .ignoresSafeArea()
             
             VStack {
                 // Prancing pony image
+                Image(.prancingpony)
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 200)
                 
                 // Currency exchange text
+                Text("Currency Exchange")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
                 
                 // Conversion section
                 HStack {
@@ -24,31 +34,56 @@ struct ContentView: View {
                         // Currency
                         HStack {
                             // Currency image
+                            Image(.silverpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                             
                             //Currency Text
+                            Text("Silver Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                         }
                         
                         // Text Field
+                        Text("Text field")
                     }
                     
                     // Equal sign
+                    Image(systemName: "equal")
+                        .font(.largeTitle)
+                        .foregroundStyle(.white)
+                        .symbolEffect(.pulse)
                     
                     // Right convesion section
                     VStack {
                         // Currency
                         HStack {
                             // Currency text
+                            Text("Gold Piece")
+                                .font(.headline)
+                                .foregroundStyle(.white)
                             
                             // Currency image
+                            Image(.goldpiece)
+                                .resizable()
+                                .scaledToFit()
+                                .frame(height: 33)
                         }
                         
                         // Text Field
+                        Text("Text field")
                     }
                 }
                 
-                // Info button
+                Spacer()
                 
+                // Info button
+                Image(systemName: "info.circle.fill")
+                    .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
+            .border(.blue)
         }
     }
 }
